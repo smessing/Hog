cd32e4150af91a90cfad003b544437437dd418f3
Samuel Messing <samuel.messing@gmail.com>
2012-04-10 15:40:30 -0400

    starting CastExpressionNode.

---
+++ Hog/src/util/ast/node/CastExpressionNode.java
@@ -0,0 +1,25 @@
+package util.ast.node;
+
+public class CastExpressionNode extends ExpressionNode {
+
+	public CastExpressionNode() {
+		this(null);
+	}
+	
+	public CastExpressionNode(Node parent) {
+		super(parent);
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int compareTo(Node arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
