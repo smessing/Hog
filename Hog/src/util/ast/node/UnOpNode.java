f3ed818c2e51934bb2b8961dcea1efe37dd340fd
Samuel Messing <samuel.messing@gmail.com>
2012-04-10 02:29:50 -0400

    starting unary op node class.

---
+++ Hog/src/util/ast/node/UnOpNode.java
@@ -0,0 +1,21 @@
+package util.ast.node;
+
+public class UnOpNode extends ExpressionNode {
+
+	protected UnOpNode(Node parent) {
+		super(parent);
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int compareTo(Node o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
