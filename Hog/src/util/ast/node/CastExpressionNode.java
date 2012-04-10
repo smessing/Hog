b009854e543eb8aca5eb410325a1653a81e64393
Samuel Messing <samuel.messing@gmail.com>
2012-04-10 15:58:23 -0400

    adding javadoc

--- Hog/src/util/ast/node/CastExpressionNode.java
+++ Hog/src/util/ast/node/CastExpressionNode.java
@@ -1,5 +1,11 @@
 package util.ast.node;
 
+/**
+ * A node representing the CastExpression nonterminal in a parse tree.
+ * 
+ * @author sam
+ *
+ */
 public class CastExpressionNode extends ExpressionNode {
 
 	public CastExpressionNode() {
--- Hog/src/util/ast/node/PostfixExpressionNode.java
+++ Hog/src/util/ast/node/PostfixExpressionNode.java
@@ -1,8 +1,12 @@
 package util.ast.node;
 
+/**
+ * A node representing the PostfixExpression nonterminal in a parse tree.
+ * 
+ * @author sam
+ *
+ */
 public class PostfixExpressionNode extends ExpressionNode {
-
-	
 	
 	public PostfixExpressionNode() {
 		this(null);
