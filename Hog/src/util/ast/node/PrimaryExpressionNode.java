e99ea48e05f0abf84391ebcc9460d51cf5441969
Samuel Messing <samuel.messing@gmail.com>
2012-04-10 15:58:33 -0400

    adding PrimaryExpressionNode.

---
+++ Hog/src/util/ast/node/PrimaryExpressionNode.java
@@ -0,0 +1,24 @@
+package util.ast.node;
+
+/**
+ * A node representing the PrimaryExpression nonterminal in a parse tree.
+ * 
+ * @author sam
+ *
+ */
+public class PrimaryExpressionNode extends ExpressionNode {
+
+	public PrimaryExpressionNode() {
+		this(null);
+	}
+	
+	public PrimaryExpressionNode(Node parent) {
+		super(parent);
+	}
+
+	@Override
+	public String getName() {
+		return "PrimaryExpressionNode<" + type.toString() + ">";
+	}
+	
+}
