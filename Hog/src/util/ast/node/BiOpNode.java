177211c81ad0b011870a0a3bfeb13321585c2f9f
Samuel Messing <samuel.messing@gmail.com>
2012-04-11 15:41:56 -0400

    refactoring constructor of BiOpNode, all non-terminals that represent binary operations (e.g. MultiplicativeExpressionNode, ArithmeticExpressionNode, etc.) will extend BiOpNode, which is now abstract.

--- Hog/src/util/ast/node/BiOpNode.java
+++ Hog/src/util/ast/node/BiOpNode.java
@@ -9,7 +9,7 @@ import java.util.List;
  * @author sam
  *
  */
-public class BiOpNode extends ExpressionNode {
+public abstract class BiOpNode extends ExpressionNode {
 	
 	public static enum OpType{
 		MINUS, PLUS, DIVIDE, TIMES, MOD
@@ -17,12 +17,10 @@ public class BiOpNode extends ExpressionNode {
 	
 	protected OpType opType;
 	
-	public BiOpNode(OpType type) {
-		this(new ArrayList<Node>(), type);
-	}
-	
-	public BiOpNode(List<Node> children, OpType type) {
-		super(children);
+	public BiOpNode(OpType type, Node left, Node right) {
+		this.children = new ArrayList<Node>();
+		this.children.add(left);
+		this.children.add(right);
 		this.opType = type;	
 	}
 	
