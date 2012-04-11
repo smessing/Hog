745a16d5d408bf492b3659e8260b8af9d3bbba21
Samuel Messing <samuel.messing@gmail.com>
2012-04-11 15:46:36 -0400

    adding commentary to BiOpNode.

--- Hog/src/util/ast/node/BiOpNode.java
+++ Hog/src/util/ast/node/BiOpNode.java
@@ -6,6 +6,11 @@ import java.util.List;
 /**
  * A node representing the Binary Operator terminals in a parse tree.
  * 
+ * Note: while not prescribed here, it is assumed that all classes that inherit from BiOpNode
+ * have a method evaluate() that returns the evaluation of the expression node. The type of this
+ * method should be determined by the type of the node (i.e. multiplicativeExpressionNodes return
+ * a number type, and booleanExpressionNodes return a boolean type).
+ * 
  * @author sam
  *
  */
