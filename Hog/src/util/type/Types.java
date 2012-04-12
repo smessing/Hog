5179adb99d92e3877e80a0e7dfbbf47a4a4cfb5c
Samuel Messing <samuel.messing@gmail.com>
2012-04-11 23:28:28 -0400

    working on types.

--- Hog/src/util/type/Types.java
+++ Hog/src/util/type/Types.java
@@ -7,13 +7,24 @@ public class Types {
 	public static enum Type {
 		BOOL, TEXT, VOID, INT, REAL, DERIVED
 	}
-	
+
 	public static enum Derived {
 		LIST, ITER
 	}
-	
+
 	public static boolean isSameType(ExpressionNode one, ExpressionNode two) {
 		throw new UnsupportedOperationException("TODO");
 	}
-	
+
+	public static Type getHigherType(Type typeOne, Type typeTwo) {
+
+		if (typeOne == Type.REAL || typeTwo == Type.REAL)
+			return Type.REAL;
+		else if (typeOne == Type.INT || typeTwo == Type.INT)
+			return Type.INT;
+		else
+			return Type.TEXT;
+
+	}
+
 }
