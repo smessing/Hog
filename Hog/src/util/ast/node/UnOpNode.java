dd3c7e75930b50c7bced6d8349a2f8d3537f9335
Samuel Messing <samuel.messing@gmail.com>
2012-04-16 14:20:55 -0400

    updating UnOpNode next.

--- Hog/src/util/ast/node/UnOpNode.java
+++ Hog/src/util/ast/node/UnOpNode.java
@@ -12,7 +12,7 @@ import util.type.Types.Type;
  * @author sam
  *
  */
-public abstract class UnOpNode extends ExpressionNode {
+public class UnOpNode extends ExpressionNode {
 	
 	// note: NONE means no unary operator applied.
 	public static enum OpType {
@@ -21,19 +21,13 @@ public abstract class UnOpNode extends ExpressionNode {
 	
 	protected OpType opType;
 	
-	public UnOpNode(ExpressionNode child, OpType opType) {
-		this.children = new ArrayList<Node>();
+	public UnOpNode(OpType opType, ExpressionNode child, Type type) {
+		super(type);
 		// note implicitly sets child.parent = this
 		this.addChild(child);
 		this.opType = opType;
 	}
 	
-	public UnOpNode(ExpressionNode child, Type type) {
-		this.children = new ArrayList<Node>();
-		this.addChild(child);
-		this.opType = OpType.CAST;
-		this.type = type;
-	}
 	
 	public OpType getOpType() {
 		return opType;
