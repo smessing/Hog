85252bdc512c86d00f45c7b2c313ace526198da5
Samuel Messing <samuel.messing@gmail.com>
2012-04-16 10:17:03 -0400

    working on FunctionNode's constructor.

--- Hog/src/util/ast/node/FunctionNode.java
+++ Hog/src/util/ast/node/FunctionNode.java
@@ -1,9 +1,18 @@
 package util.ast.node;
 
+import util.type.Types;
 import back_end.Visitor;
 
-public class FunctionNode extends SectionNode {
+public class FunctionNode extends ExpressionNode {
 
+	protected ParametersNode parameters;
+	
+	public FunctionNode(Types.Type type, ParametersNode parameters, SectionNode instructions) {
+		super(type);
+		this.parameters = parameters;
+		this.addChild(instructions);
+	}
+	
 	@Override
 	public void accept(Visitor v) {
 		v.visit(this);
