f3d8acfac9718974d7dc6e70cb24c3d02752f57e
Samuel Messing <samuel.messing@gmail.com>
2012-04-15 22:31:21 -0400

    adding ElseIfStatementNode and ElseStatementNode. Currently, ElseStatementNode sublcasses ElseIfStatementNode, since and else statement is essentially an else if statement where the if expression is trivially true.

---
+++ Hog/src/util/ast/node/ElseIfStatementNode.java
@@ -0,0 +1,11 @@
+package util.ast.node;
+
+import java.util.List;
+
+public class ElseIfStatementNode extends StatementNode {
+
+	ElseIfStatementNode(List<Node> children) {
+		super(children);
+	}
+
+}
