73c0b06a1761e96a880093ec3308cd1940d315dc
Samuel Messing <samuel.messing@gmail.com>
2012-04-16 14:24:57 -0400

    remoivng classes, fixing visitor as a result.

--- Hog/src/back_end/Visitor.java
+++ Hog/src/back_end/Visitor.java
@@ -5,7 +5,6 @@ import util.ast.node.*;
 public abstract class Visitor {
 	
 	public abstract void visit(BiOpNode node);
-	public abstract void visit(CastExpressionNode node);
 	public abstract void visit(ConstantNode node);
 	public abstract void visit(ExpressionNode node);
 	public abstract void visit(IdNode node);
@@ -13,10 +12,8 @@ public abstract class Visitor {
 	public abstract void visit(MockNode node);
 	public abstract void visit(Node node);
 	public abstract void visit(ParametersNode node);
-	public abstract void visit(PostfixExpressionNode node);
 	public abstract void visit(PrimaryExpressionNode node);
 	public abstract void visit(RelationalExpressionNode node);
-	public abstract void visit(UnaryExpressionNode node);
 	public abstract void visit(UnOpNode node);
 
 }
