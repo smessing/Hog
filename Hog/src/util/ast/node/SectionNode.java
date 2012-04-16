b0b5cdb593f1ba63a5e8d7c17399126478105116
Samuel Messing <samuel.messing@gmail.com>
2012-04-16 10:13:28 -0400

    working on SectionNode's constructor.

--- Hog/src/util/ast/node/SectionNode.java
+++ Hog/src/util/ast/node/SectionNode.java
@@ -1,8 +1,18 @@
 package util.ast.node;
 
+import java.util.ArrayList;
+
 import back_end.Visitor;
 
 public class SectionNode extends Node {
+	
+	protected SectionTypeNode type;
+	
+	public SectionNode(SectionTypeNode type, StatementNode list) {
+		this.children = new ArrayList<Node>();
+		this.addChild(list);
+		this.type = type;
+	}
 	
 	@Override
 	public void accept(Visitor v) {
