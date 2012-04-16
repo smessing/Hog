65fe0886fd2ee55fd6ba6688cd1d791aeb100507
Samuel Messing <samuel.messing@gmail.com>
2012-04-15 23:05:49 -0400

    adding Section node.

---
+++ Hog/src/util/ast/node/SectionNode.java
@@ -0,0 +1,24 @@
+package util.ast.node;
+
+import back_end.Visitor;
+
+public class SectionNode extends Node {
+
+	@Override
+	public void accept(Visitor v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		return "SectionNode";
+	}
+
+	@Override
+	public int visitorTest(Visitor v) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
