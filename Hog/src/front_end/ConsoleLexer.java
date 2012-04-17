de7bd4c7fad44c168990fd60a9ba4bc2f11569fb
Samuel Messing <samuel.messing@gmail.com>
2012-04-17 02:16:35 -0400

    grammar's coming along, but want to try something.

--- Hog/src/front_end/ConsoleLexer.java
+++ Hog/src/front_end/ConsoleLexer.java
@@ -39,7 +39,7 @@ public class ConsoleLexer {
 	    try {
 	        // Parser p = new Parser(new Lexer(System.in));
 	    	Parser p = new Parser(new Lexer(fileReader));
-	    	p.debug_parse();
+	    	p.parse();
 	    	/*System.out.println(p.scan());
 	    	System.out.println(p.scan());
 	    	System.out.println(p.scan());
