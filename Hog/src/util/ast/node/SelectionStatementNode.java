8b8a92d7062bb97fbdfc4553c24e63590cc5dee5
Samuel Messing <samuel.messing@gmail.com>
2012-04-20 18:44:46 -0400

    trying to refactor if-else statements.

--- Hog/src/util/ast/node/SelectionStatementNode.java
+++ Hog/src/util/ast/node/SelectionStatementNode.java
@@ -1,25 +1,13 @@
 package util.ast.node;
 
-import util.type.Types;
-
 public class SelectionStatementNode extends StatementNode {
 
-	protected StatementNode ifCondTrue;
-	protected StatementNode ifCondFalse;
-	protected ExpressionNode condition;
 	
-	public SelectionStatementNode(ExpressionNode condition, StatementNode ifCondTrue, StatementNode ifCondFalse) {
-		super();
-		this.ifCondTrue = ifCondTrue;
-		this.ifCondFalse = ifCondFalse;
-		if (condition == null) {
-			// if condition was null, we make it trivially true:
-			condition = new ConstantNode(Types.Type.BOOL, "true");
-		}
+	ExpressionNode condition;
+	
+	SelectionStatementNode(ExpressionNode condition) {
 		this.condition = condition;
 		this.addChild(condition);
-		this.addChild(ifCondTrue);
-		this.addChild(ifCondFalse);
 	}
 	
 	public ExpressionNode getCondition() {
