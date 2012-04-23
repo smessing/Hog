10a5d70ddfa5d92043a9cb2be299b05c5a2e4039
Samuel Messing <samuel.messing@gmail.com>
2012-04-23 11:51:57 -0400

    adding files that somehow got deleted.

---
+++ Hog/src/util/ast/node/PostfixExpressionNode.java
@@ -0,0 +1,62 @@
+package util.ast.node;
+
+import util.type.Types.Type;
+import back_end.Visitor;
+
+/**
+ * Represents PostFixExpression productions for the following types of postfix expressions:
+ *   array indexing, 
+ *   method calls with no params,
+ *   method calls with params 
+ *   function calls
+ *   
+ *  Can check which it represents by calling getPostFixType
+ *  
+ * @author ben
+ *
+ */
+public class PostfixExpressionNode extends ExpressionNode {
+
+	public static enum PostfixType {
+		ARRAY_INDEX, METHOD_NO_PARAMS, METHOD_WITH_PARAMS, FUNCTION_CALL
+	}
+
+	protected PostfixType postfixType;
+
+	// for array indexing, method calls with no params, function calls
+	public PostfixExpressionNode(PostfixType postfixType, ExpressionNode postfixExpr, 
+			                     ExpressionNode expr) {
+		super(Type.UNKNOWN);
+		this.postfixType = postfixType;
+		this.addChild(postfixExpr);
+		this.addChild(expr);
+	}
+
+	// for method calls with params
+	public PostfixExpressionNode(PostfixType postFixType, ExpressionNode postfixExpr,
+			                     ExpressionNode id, ExpressionNode argsList) {
+		super(Type.UNKNOWN);
+		this.postfixType = postFixType;
+		this.addChild(postfixExpr);
+		this.addChild(id);
+		this.addChild(argsList);
+	}
+
+
+	public PostfixType getPostfixType() {
+		return postfixType;
+	}
+
+	@Override
+	public String getName() {
+		return postfixType.toString() + "<" + this.getTypeName() + ">";
+	}
+
+	@Override
+	public void accept(Visitor v) {
+		v.visit(this);
+	}
+
+
+
+}
