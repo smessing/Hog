977cdfe4abd88d11c91289917246686b1b0a1afa
Samuel Messing <samuel.messing@gmail.com>
2012-04-25 16:16:11 -0400

    fleshing out SwitchStatementNode.

--- Hog/src/util/ast/node/SwitchStatementNode.java
+++ Hog/src/util/ast/node/SwitchStatementNode.java
@@ -1,29 +1,41 @@
 package util.ast.node;
 
+import back_end.Visitor;
+
 /**
  * 
  * @author sam
- *
+ * 
  */
 public class SwitchStatementNode extends StatementNode {
 
 	protected ExpressionNode expression;
 	protected StatementListNode statements;
-	
-	public SwitchStatementNode(ExpressionNode expression, StatementListNode statements) {
+
+	public SwitchStatementNode(ExpressionNode expression,
+			StatementListNode statements) {
 		this.expression = expression;
 		this.statements = statements;
 		this.addChild(expression);
 		this.addChild(statements);
 	}
-	
+
 	public ExpressionNode getExpression() {
 		return this.expression;
 	}
-	
+
 	public StatementListNode getStatements() {
 		return this.statements;
 	}
-	
-	
+
+	@Override
+	public String getName() {
+		return "SwitchStatementNode";
+	}
+
+	@Override
+	public void accept(Visitor v) {
+		v.visit(this);
+	}
+
 }
