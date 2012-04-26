20e028b01b0a63e7c9801d5c3017589a95f29fcf
Benjamin Rapaport <benjamin.rapaport@gmail.com>
2012-04-25 23:25:57 -0400

    adding type node and type enum for reserved words in sumbol table

--- Hog/src/util/type/Types.java
+++ Hog/src/util/type/Types.java
@@ -16,6 +16,10 @@ public class Types {
 	public static enum Exception {
 		FILE_NOT_FOUND, FILE_LOAD, ARRAY_OUT_OF_BOUNDS, INCORRECT_ARGUMENT, TYPE_MISMATCH, NULL_REFERENCE, ARITHMETIC
 	}
+	
+	public static enum Flags {
+		RESERVED_WORD, CHECK_INNER_TYPE
+	}
 
 	public static boolean isSameType(ExpressionNode one, ExpressionNode two) {
 		throw new UnsupportedOperationException("TODO");
