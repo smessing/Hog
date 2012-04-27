4021e803c2a82497d65aa4bb4d995bd6ee9ebd34
Samuel Messing <samuel.messing@gmail.com>
2012-04-27 18:09:57 -0400

    starting Parser tests.

---
+++ Hog/src/front_end/ParserTester.java
@@ -0,0 +1,36 @@
+package front_end;
+
+import static org.junit.Assert.*;
+
+import java.io.StringReader;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import util.ast.node.ProgramNode;
+
+
+/**
+ * Tests basic functionality of Parser.java.
+ * 
+ * 
+ * @author sam
+ *
+ */
+public class ParserTester {
+	
+	@Test
+	public void firstTest() throws Exception {
+		String text = "2 + 5";
+		StringReader stringReader = new StringReader(text);
+		Lexer lexer = new Lexer(stringReader);
+		Parser parser = new Parser(lexer);
+		ProgramNode root = (ProgramNode) parser.parse().value;
+		System.out.println(root);
+	}
+	
+
+}
